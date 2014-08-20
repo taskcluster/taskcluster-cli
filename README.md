43cb516be4f698ec471283e67e24c7b368b18560
Greg Arndt <garndt@mozilla.com>
2014-08-20 14:17:25 -0500

    Version 0.1 - refactored to use promises and added command line options

--- README.md
+++ README.md
@@ -1,21 +1,50 @@
 # taskcluster-cli
+that you'd normally have to do by hand. There are tasks that are common
+Taskcluster CLI is a command line interface for creating and managing 
+tasks submitted to taskcluster.
 
-## Requirements
+# Requirements
 
- - node 0.11 or greater
+ - node 0.11x or greater
 
-## Usage
+# Installation
+1. Clone a copy of this repo
+2. Run `npm install <path to repo>` to install taskcluster-cli and required dependencies.
+3. If not installing globally, add the node_modules/.bin path to your shell.
 
-First you need to export your taskcluster credentials:
+# Configuration
+
+Add taskcluster credential environment variables.  This is best done in your shell profile.
 
 ```sh
+export TASKCLUSTER_TASK_OWNER=...
 export TASKCLUSTER_CLIENT_ID=...
 export TASKCLUSTER_ACCESS_TOKEN=...
 ```
 
-This is best done in you zsh/bash/sh profile...
+# Usage
 
 ```sh
-# from the root of the project
-node --harmony bin/taskcluster-run.js <docker_image> <args....>
+taskcluster run --provisioner-id=<instance provisioner> --worker-type=<worker type> <image> <commands>
+```
+
+# Example
+
+Create a task that will count the number of files in a directory on Ubuntu 14.04.
+
+```sh
+taskcluster run --provisioner-id=aws-provisioner --worker-type=cli ubuntu:14.04 'find /bin -type f -print | wc -l'
+
+Task Created.
+Task ID: xquu2goHS3-pexVC9w4dmw
+Task State: Pending
+Task Completed
+[taskcluster] taskId: xquu2goHS3-pexVC9w4dmw, workerId: i-efa604e0 
+
+ubuntu:14.04 exists in the cache.
+find /bin -type f -print | wc -l
++ find /bin -type f -print
++ wc -l
+100
+[taskcluster] Successful task run with exit code: 0 completed in 0.984 seconds
 ```
--- bin/taskcluster-run.js
+++ bin/taskcluster-run.js
@@ -1,121 +1,183 @@
-var taskcluster = require('taskcluster-client');
-var slugid = require('slugid');
-var debug = require('debug')('taskcluster-client');
-var co = require('co');
-
-var LogStream = require('taskcluster-logstream');
-var Listener = require('taskcluster-client').Listener;
+var fs = require('fs');
 var Promise = require('promise');
-var TaskFactory = require('taskcluster-task-factory/task');
+var slugid = require('slugid');
+var dotenv = require('dotenv');
+var _ = require('lodash');
 
-var queueEvents = new (require('taskcluster-client').QueueEvents);
-var schedulerEvents = new (require('taskcluster-client').SchedulerEvents);
+var taskcluster = require('taskcluster-client');
+var debug = require('debug')('taskcluster-cli:run');
+var TaskFactory = require('taskcluster-task-factory/task');
+var LogStream = require('taskcluster-logstream');
+
+var Listener = taskcluster.Listener;
+var queueEvents = new taskcluster.QueueEvents;
 var queue = new taskcluster.Queue();
-var image = process.argv[2];
-var commands = process.argv.slice(3);
+
+var listener;
+var taskComplete = false;
 
 var LOG_NAME = 'public/logs/terminal_live.log';
+var taskOwner = process.env.TASKCLUSTER_TASK_OWNER;
 
-function waitForEvent(listener, event) {
-  return new Promise(function(accept, reject) {
-    listener.on(event, function(message) {
-      accept(message);
-    });
-  });
+var yargs = require('yargs')
+  .usage('Run a task within the task cluster')
+  .example('taskcluster run', '-e BUILD_OPTS=debug ubuntu:14.04 \'make -j=5\'')
+  .demand(['provisioner-id', 'worker-type'])
+  .alias('e', 'env')
+  .describe('e', 'Environment variable to apply on worker')
+  .describe('env-file', 'Environment variable file to apply on worker')
+  .describe('provisioner-id', 'Provisioner ID. Example: aws-provisioner')
+  .describe('worker-type', 'Worker Type. Example: \'cli\'')
+
+var args = yargs.argv
+
+if(args._.length < 2) {
+  console.error('Error: Must supply an image and command\n')
+  console.log(yargs.help());
+  process.exit(1);
+} else {
+  var image = args._[0];
+  var command = ['/bin/bash', '-cvex'].concat(args._.slice(1));
 }
 
-function* createTask(taskId, taskConfig) {
-  // XXX: This is just a hack really so the validator does not complain.
-  taskConfig.taskGroupId = taskId;
-  var task = TaskFactory.create(taskConfig);
-  debug('post to queue %j', task);
-  return yield queue.createTask(taskId, task);
+if(!taskOwner) {
+  console.error('Error: TASKCLUSTER_TASK_OWNER not specified\n');
+  console.log(yargs.help());
+  process.exit(1);
+}
+
+// Command line arguments should take precedence over those listed in the env file.
+// Allows selective overwriting of env variables
+var env = {};
+if (args.env) {
+  var joinedEnvs = args.env.join('\n')
+  env = dotenv.parse(joinedEnvs);
+}
+
+if (args.envFile) {
+  if (fs.existsSync(args.envFile)) {
+    var file = fs.readFileSync(args.envFile);
+    env = _.defaults(env, dotenv.parse(file));
+  } else {
+    console.error('Environment file does not exist at the location provided.');
+    process.exit(1);
+  }
+}
+
+function endWhenTasksComplete(stream) {
+  if (!stream && taskComplete) {
+    listener.close();
+  } else {
+    setTimeout(function (stream) { endWhenTasksComplete(stream); }, 1);
+  }
 }
 
 function displayLog(taskId, runId) {
   var signedUrl = queue.buildSignedUrl(
-    queue.getArtifact, taskId, runId, LOG_NAME, { expiration: 60 * 100 }
+      queue.getArtifact, taskId, runId, LOG_NAME, {expiration: 60 * 100}
   );
   var stream = new LogStream(signedUrl);
   stream.pipe(process.stdout);
+  endWhenTasksComplete(stream);
 }
 
-function* hasLog(taskId, runId) {
-  var list = yield queue.listArtifacts(taskId, runId);
-  return list.artifacts.some(function(item) {
-    return item.name === LOG_NAME;
-  });
-}
+function handleEvent(message) {
+  var payload = message.payload;
+  var taskId = payload.status.taskId;
+  var runId = payload.runId;
 
-function* handleEvents(listener) {
-  var event;
-  while (event = yield waitForEvent(listener, 'message')) {
-    var payload = event.payload;
-    var taskId = payload.status.taskId;
-    var runId = payload.runId;
-
-    switch (payload.status.state) {
-      case 'completed':
-        yield listener.close();
-        break;
-      case 'running':
-        var list;
-        // wait until we get artifacts...
-        while (!(yield hasLog(taskId, runId))) {}
+  switch (payload.status.state) {
+    case 'pending':
+      console.log("Task Created.\nTask ID: %s\nTask State: Pending", taskId);
+      break;
+    case 'running':
+      if (message.exchange.indexOf('artifact-created') > -1 && payload.artifact.name == LOG_NAME) {
         displayLog(taskId, runId);
-    }
+      }
+      break;
+    case 'completed':
+      if (payload.success) {
+        console.log('Task Completed');
+        taskComplete = true;
+        break;
+      }
+    case 'failed':
+      if (payload.status.state == 'completed') {
+        result = 'Task Completed Unsuccessfully';
+      } else {
+        taskComplete = true;
+        result = 'Task Failed';
+      }
+      debug("Message: %j", message);
   }
 }
 
-co(function* () {
-  // Be lazy and terrible and assume you want shell style...
-  if (commands) commands = ['/bin/sh', '-c'].concat(commands);
+function buildTaskRequest(taskId) {
+    var creationDate = new Date();
+    var deadlineDate = new Date(creationDate)
+    deadlineDate.setHours(deadlineDate.getHours() + 24);
 
-  var taskId = slugid.v4();
-
-  // Create and bind the listener which will notify us when the worker
-  // completes a task.
-  var listener = new Listener({
-    connectionString: (yield queue.getAMQPConnectionString()).url
-  });
-
-  yield listener.bind(queueEvents.taskRunning({
-    taskId: taskId
-  }));
-
-  yield listener.bind(queueEvents.taskCompleted({
-    taskId: taskId
-  }));
-
-  yield listener.connect();
-  yield listener.resume();
-
-  // Begin listening at the same time we create the task to ensure we get the
-  // message at the correct time.
-  var creation = yield [
-    handleEvents(listener),
-    createTask(taskId, {
-      workerType: 'v2',
-      provisionerId: 'aws-provisioner',
-      schedulerId: 'taskcluster-cli',
-
-      metadata: {
-        owner: 'jlal@mozilla.com'
+    var task = {
+      'provisionerId': args['provisioner-id'],
+      'workerType': args['worker-type'],
+      'schedulerId': 'taskcluster-cli',
+      'taskGroupId': taskId,
+      'routes': [],
+      'retries': 1,
+      'priority': 5,
+      'created': creationDate,
+      'deadline': deadlineDate,
+      'scopes': [],
+      'payload': {
+        'image': image,
+        'command': command,
+        'env': env,
+        'maxRunTime': 600
       },
-
-      payload: {
-        image: image,
-        command: commands
+      'metadata': {
+        'owner': taskOwner,
+        'name': '',
+        'description': '',
+        'source': 'http://localhost'
+      },
+      'tags': {
+        'misc_info': 'task created by cli'
       }
-    }),
-  ];
+    };
+    debug("Task Payload: %j", task);
 
-  try {
-    yield listener.close();
-  } catch(e) {
-    console.log('error during close:', e);
+  return task;
+}
+
+function createListener(connectionString, taskId, eventHandler) {
+  listener = new Listener({
+    connectionString: connectionString,
+  });
+  listener.bind(queueEvents.taskPending({taskId: taskId}));
+  listener.bind(queueEvents.taskRunning({taskId: taskId}));
+  listener.bind(queueEvents.taskCompleted({taskId: taskId}));
+  listener.bind(queueEvents.artifactCreated({taskId: taskId}));
+  listener.on('message', handleEvent);
+  return listener;
+}
+
+queue.getAMQPConnectionString().then(function(result) {
+  var taskId = slugid.v4();
+  var task = buildTaskRequest(taskId);
+  var listener = createListener(result.url, taskId, handleEvent);
+
+  return listener.resume().then(function () {
+    return queue.createTask(taskId, task)
+  });
+}).catch(function(error) {
+  if(error.body) {
+    console.error("Message: %s", error.body.message);
+    console.error("Error: %j", error.body.error);
+  } else {
+    console.log(error);
   }
-})(function(err) {
-  console.log('ERR: ', err);
+  if (error.stack) {;
+    console.error(error.stack);
+  }
   process.exit(1);
 });
