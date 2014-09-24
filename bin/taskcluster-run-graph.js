a5ed6a99c0deb930caa51a649dda102b329ea340
James Lal <james@lightsofapollo.com>
2014-09-24 13:09:16 -0700

    Update boolean usage & add stdin support

--- bin/taskcluster-run-graph.js
+++ bin/taskcluster-run-graph.js
@@ -12,25 +12,18 @@ var scheduler = new taskcluster.Scheduler();
 
 var yargs = require('yargs')
   .usage('Create a taskcluster graph from a file')
-  .example('taskcluster run-graph', 'graph.json --open')
+  .example('taskcluster run-graph', 'graph.json')
+  .example('taskcluster run-graph --no-open', 'graph.json')
   .options('open', {
+    boolean: true,
+    default: true,
     describe: 'When true open the graph in the task graph inspector',
-    default: true
   })
   .options('dump', {
+    boolean: true,
+    default: false,
     describe: 'When true dump the task graph to stdout',
-    default: true
   });
-
-var args = yargs.argv
-var graph = args._[0];
-
-// TODO: Allow graph from stdin
-if (!graph || !fs.existsSync(graph)) {
-  console.error('Error: Task graph must be provided.');
-  console.log(yargs.help());
-  process.exit(1);
-}
 
 /**
 Figure out what format the graph is then load it and return the object.
@@ -50,36 +43,70 @@ function loadGraph(pathName) {
   }
 }
 
-var graphContent = loadGraph(graph);
+function runGraph(graphContent) {
+  // Most fields can be directly embedded in the graph but some must be
+  // dynamically generated such as the taskId and deadline...
+  graphContent.tasks = graphContent.tasks.map(function(node) {
+    node.taskId = node.taskId || slugid.v4();
+    node.task.created = node.created || new Date().toJSON();
+    if (!node.task.deadline) {
+      node.task.deadline = new Date();
+      node.task.deadline.setHours(node.task.deadline.getHours() + 24);
+    }
+    return node;
+  });
 
-// Most fields can be directly embedded in the graph but some must be
-// dynamically generated such as the taskId and deadline...
-graphContent.tasks = graphContent.tasks.map(function(node) {
-  node.taskId = node.taskId || slugid.v4();
-  node.task.created = node.created || new Date().toJSON();
-  if (!node.task.deadline) {
-    node.task.deadline = new Date();
-    node.task.deadline.setHours(node.task.deadline.getHours() + 24);
+  // Schedule the task
+  var graphId = slugid.v4();
+  var inspectorUrl = 'http://docs.taskcluster.net/tools/task-graph-inspector/#';
+  inspectorUrl += graphId;
+
+  // Dump the graph first so its easy to inspect for errors...
+  if (args.dump) {
+    process.stderr.write(JSON.stringify(graphContent, null, 2));
   }
-  return node;
-});
 
-// Schedule the task
-var graphId = slugid.v4();
-var inspectorUrl = 'http://docs.taskcluster.net/tools/task-graph-inspector/#';
-inspectorUrl += graphId;
-
-// Dump the graph first so its easy to inspect for errors...
-if (args.dump) {
-  process.stdout.write(JSON.stringify(graphContent, null, 2));
+  scheduler.createTaskGraph(graphId, graphContent).then(function() {
+    if (args.open) {
+      open(inspectorUrl);
+    }
+    process.stdout.write(graphId);
+  }).catch(function(err) {
+    console.error('Taskgraph creation error', err.toString());
+    process.stderr.write(JSON.stringify(err.body, null, 2));
+    process.exit(1);
+  });
 }
 
-scheduler.createTaskGraph(graphId, graphContent).then(function() {
-  if (args.open) {
-    open(inspectorUrl);
-  }
-}).catch(function(err) {
-  console.error('Taskgraph creation error', err.toString());
-  process.stderr.write(JSON.stringify(err.body, null, 2));
+var args = yargs.argv
+var graph = args._[0];
+
+if (graph && !fs.existsSync(graph)) {
+  console.error('Error: graph file "%s" does not exist', graph);
+  console.error(yargs.help());
   process.exit(1);
-});
+}
+
+if (graph) {
+  // Run graph from a file...
+  runGraph(loadGraph(graph));
+} else {
+  var graphContent = '';
+  // Run graph from stdin...
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('readable', function() {
+    var chunk;
+    while (chunk = process.stdin.read()) graphContent += chunk;
+  });
+  process.stdin.once('end', function() {
+    var graph;
+    try {
+      graph = JSON.parse(graphContent);
+      runGraph(graph);
+    } catch(e) {
+      console.error("Failed to read JSON from stdin: %j", graph);
+      console.error(yargs.help());
+      process.exit(1);
+    }
+  });
+}
