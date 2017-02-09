0ee6f38daea0b3f27359432e01337e996513e89b
AyubMohamed <ayubmohamed94@gmail.com>
2017-02-09 16:13:16 -0500

    Return log's of a completed task, for live tasks it's much more complicated

--- task-log/task_log.go
+++ task-log/task_log.go
@@ -1,6 +1,15 @@
 package taskLog
 
-import "github.com/taskcluster/taskcluster-cli/extpoints"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/taskcluster/taskcluster-cli/apis"
+	"github.com/taskcluster/taskcluster-cli/extpoints"
+)
 
 type taskLog struct{}
 
@@ -13,17 +22,56 @@ func (taskLog) ConfigOptions() map[string]extpoints.ConfigOption {
 }
 
 func (taskLog) Summary() string {
-	return "Outputs the logs for <taskId> as generated, and exits when completes."
+	return "Outputs the logs for <taskID> as generated, and exits when completes."
 }
 
 func (taskLog) Usage() string {
-	usage := "Usage: taskcluster task-log <taskId>\n"
+	usage := "Usage: taskcluster task-log <taskID>\n"
 	return usage
 }
 
 func (taskLog) Execute(context extpoints.Context) bool {
-	// TODO :
-	// While task status is still running
-	// print logs
+	taskID := context.Arguments["<taskID>"].(string)
+
+	// Get route from services.go
+	route := apis.Services["Queue"].BaseURL
+	for _, entry := range apis.Services["Queue"].Entries {
+		if entry.Name == "getLatestArtifact" {
+			route += entry.Route
+			break
+		}
+	}
+
+	route = strings.Replace(route, "<taskId>", taskID, 1)
+	route = strings.Replace(route, "<name>", "public/logs/live_backing.log", 1)
+
+	body, _ := makeGetRequest(route)
+
+	// Check if we got any errors, we are not expecting a json response.
+	var raw map[string]interface{}
+	json.Unmarshal(body, &raw)
+
+	if len(raw) != 0 {
+		// Error, most likely with the taskID
+		if _, ok := raw["message"]; ok {
+			panic(raw["message"])
+		}
+		return false
+	}
+
+	fmt.Println(string(body))
 	return true
+
+}
+
+func makeGetRequest(path string) (b []byte, err error) {
+	resp, err := http.Get(path)
+	if err != nil {
+		panic("Error making request to " + path)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return body, err
 }
