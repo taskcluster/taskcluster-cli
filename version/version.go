25f6d240467c0ff16cc96e2ca90e6749d98360d1
t0xicCode <xavier.l@magnax.ca>
2017-02-11 11:47:36 -0500

    Migrated version command

--- version/version.go
+++ version/version.go
@@ -3,37 +3,28 @@ package version
 import (
 	"fmt"
 
-	"github.com/taskcluster/taskcluster-cli/extpoints"
+	"github.com/spf13/cobra"
+
+	"github.com/taskcluster/taskcluster-cli/root"
 )
 
-// VersionNumber is a string with the formatted version data.
-var VersionNumber = fmt.Sprintf("taskcluster (TaskCluster CLI) version %d.%d.%d", 1, 0, 0)
+var (
+	// Command is the cobra command representing the version subtree.
+	Command = &cobra.Command{
+		Use:   "version",
+		Short: "Prints the TaskCluster version.",
+		Run:   printVersion,
+	}
+
+	// VersionNumber is a formatted string with the version information.
+	VersionNumber = fmt.Sprintf("%d.%d.%d", 1, 0, 0)
+)
+
 
 func init() {
-	extpoints.Register("version", version{})
+	root.Command.AddCommand(Command)
 }
 
-type version struct{}
-
-func (version) ConfigOptions() map[string]extpoints.ConfigOption {
-	return nil
-}
-
-func (version) Summary() string {
-	return "Prints the TaskCluster version."
-}
-
-func usage() string {
-	return `Usage:
-  taskcluster version
-`
-}
-
-func (version) Usage() string {
-	return usage()
-}
-
-func (version) Execute(context extpoints.Context) bool {
-	fmt.Println(VersionNumber)
-	return true
+func printVersion(_ *cobra.Command, _ []string) {
+	fmt.Printf("taskcluster (TaskCluster CLI) version %s\n", VersionNumber)
 }
