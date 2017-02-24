aac78ab974f9c1ea8e493561810f8a323bb39c94
t0xicCode <xavier.l@magnax.ca>
2017-02-24 12:01:56 -0500

    Migrated task-log into task log

--- task/fetchers.go
+++ task/fetchers.go
@@ -1,26 +1,16 @@
 package task
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io"
+	"net/http"
 
 	"github.com/spf13/pflag"
 	tcclient "github.com/taskcluster/taskcluster-client-go"
 	"github.com/taskcluster/taskcluster-client-go/queue"
 )
-
-// Executor represents the function interface of the task subcommand.
-type Executor func(credentials *tcclient.Credentials, args []string, out io.Writer, flagSet *pflag.FlagSet) error
-
-// getRunStatusString takes the state and resolved strings and crafts a printable summary string.
-func getRunStatusString(state, resolved string) string {
-	if resolved != "" {
-		return fmt.Sprintf("%s '%s'", state, resolved)
-	}
-
-	return state
-}
 
 // runStatus gets the status of run(s) of a given task.
 func runStatus(credentials *tcclient.Credentials, args []string, out io.Writer, flagSet *pflag.FlagSet) error {
@@ -125,64 +115,27 @@ func runArtifacts(credentials *tcclient.Credentials, args []string, out io.Write
 	return nil
 }
 
-// runCancel cancels the runs of a given task.
-func runCancel(credentials *tcclient.Credentials, args []string, out io.Writer, _ *pflag.FlagSet) error {
-	q := queue.New(credentials)
+func runLog(credentials *tcclient.Credentials, args []string, out io.Writer, flagSet *pflag.FlagSet) error {
 	taskID := args[0]
 
-	c, err := q.CancelTask(taskID)
+	path := "https://queue.taskcluster.net/v1/task/" + taskID + "/artifacts/public/logs/live.log"
+
+	resp, err := http.Get(path)
 	if err != nil {
-		return fmt.Errorf("could not cancel the task %s: %v", taskID, err)
+		return fmt.Errorf("Error making request to %v: %v", path, err)
+	}
+	defer resp.Body.Close()
+
+	// Read line by line for live logs.
+	// This will also print the error message for failed requests.
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
 	}
 
-	run := c.Status.Runs[len(c.Status.Runs)-1]
-	fmt.Fprintln(out, getRunStatusString(run.State, run.ReasonResolved))
-	return nil
-}
-
-// runRerun re-runs a given task.
-func runRerun(credentials *tcclient.Credentials, args []string, out io.Writer, _ *pflag.FlagSet) error {
-	q := queue.New(credentials)
-	taskID := args[0]
-
-	c, err := q.RerunTask(taskID)
-	if err != nil {
-		return fmt.Errorf("could not rerun the task %s: %v", taskID, err)
+	if resp.StatusCode/100 != 2 {
+		return fmt.Errorf("Received unexpected response code %v", resp.StatusCode)
 	}
 
-	run := c.Status.Runs[len(c.Status.Runs)-1]
-	fmt.Fprintln(out, getRunStatusString(run.State, run.ReasonResolved))
-	return nil
-}
-
-// runComplete completes a given task.
-func runComplete(credentials *tcclient.Credentials, args []string, out io.Writer, _ *pflag.FlagSet) error {
-	q := queue.New(credentials)
-	taskID := args[0]
-
-	s, err := q.Status(taskID)
-	if err != nil {
-		return fmt.Errorf("could not get the status of the task %s: %v", taskID, err)
-	}
-
-	c, err := q.ClaimTask(taskID, fmt.Sprint(len(s.Status.Runs)-1), &queue.TaskClaimRequest{
-		WorkerGroup: s.Status.WorkerType,
-		WorkerID:    "taskcluster-cli",
-	})
-	if err != nil {
-		return fmt.Errorf("could not claim the task %s: %v", taskID, err)
-	}
-
-	wq := queue.New(&tcclient.Credentials{
-		ClientID:    c.Credentials.ClientID,
-		AccessToken: c.Credentials.AccessToken,
-		Certificate: c.Credentials.Certificate,
-	})
-	r, err := wq.ReportCompleted(taskID, fmt.Sprint(c.RunID))
-	if err != nil {
-		return fmt.Errorf("could not complete the task %s: %v", taskID, err)
-	}
-
-	fmt.Fprintln(out, getRunStatusString(r.Status.Runs[c.RunID].State, r.Status.Runs[c.RunID].ReasonResolved))
 	return nil
 }
