ff91ec820938343056d93154108203d12b0a3f86
Yannick Schoerner <yannick.schoerner@gmail.com>
2017-03-28 14:55:37 +0200

    Corrected executable name

--- cmds/root/command.go
+++ cmds/root/command.go
@@ -5,7 +5,7 @@ import "github.com/spf13/cobra"
 var (
 	// Command is the root of the command tree.
 	Command = &cobra.Command{
-		Use:   "taskcluster-cli",
+		Use:   "taskcluster",
 		Short: "TaskCluster CLI client.",
 		Long:  "A command-line interface to TaskCluster - see https://docs.taskcluster.net.",
 	}
