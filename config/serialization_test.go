a5e6c5bd373e5e6eb88bb5c80cb84209cb57d0a8
lteigrob <lteigrob@gmail.com>
2017-04-03 14:23:57 -0600

    Start Save() testing

--- config/serialization_test.go
+++ config/serialization_test.go
@@ -1,32 +1,45 @@
 package config
 
 import (
+    "os"
 	"strings"
 	"testing"
 
 	assert "github.com/stretchr/testify/require"
 )
 
-func TestLoad(t *testing.T) {
+func TestLoadAndSave(t *testing.T) {
+  
+    // Create tester config data and option definitions
     const configSample = `
                           cmd:
-                          foo: bar
+                            foo: bar
                          `
-
     options := map[string]OptionDefinition{
-        "foo": OptionDefinition{},
+       "foo": OptionDefinition{},
     }
+    
+    // Register the fake config options
     RegisterOptions("cmd", options)
-    r := strings.NewReader(configSample)
-    m, err := Load(r)
+    
+    // Call Load() with the test data
+    configSampleReader := strings.NewReader(configSample)
+    configMap, err := Load(configSampleReader)
     assert.NoError(t, err)
-    _, ok := m["cmd"]
+    
+    // Compare Load() output and config object
+    _, ok := configMap["cmd"]
     assert.True(t, ok)
-    _, ok = m["cmd"]["foo"]
+    _, ok = configMap["cmd"]["foo"]
     assert.True(t, ok)
-  
-}
 
-func TestSave(t *testing.T){
-	// to do 
+    // Test Save() with results from Load()
+    configWriter,_ := os.Create(configSample)
+    err = Save(configMap, configWriter)
+    assert.NoError(t,err)
+
+    // assert.Equal(configSampleReader, fileIJustMade) 
+
+    //TODO Remove the saved file after we've tested 
+    // Separate out Save and Load testing
 }
